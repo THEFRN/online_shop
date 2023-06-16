1e30e65cc57993c697bab1742b9d72f980368fc8
Ahmad <farhangian.ahmad@gmail.com>
2023-06-16 22:53:59 +0430

    created and connected custom user

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Accounts Config
+AUTH_USER_MODEL = 'accounts.CustomUser'
