c1fd98deff230f5531c30ce2831b1dd52fd5642f
Ahmad <farhangian.ahmad@gmail.com>
2023-06-16 23:07:45 +0430

    created migrations and commited migrate to DB for accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import UserChangeForm, UserCreationForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = ('email', 'username')
+
+# admin.site.register(CustomUser, CustomUserAdmin)
