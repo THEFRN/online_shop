24f95e07f1dd0ce3847cb89a4487e950d19d09d0
Ahmad <farhangian.ahmad@gmail.com>
2023-07-03 19:56:32 +0330

    Creaetd products' models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.shortcuts import reverse
 
-# Create your models here.
+
+class Products(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=1)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        pass
